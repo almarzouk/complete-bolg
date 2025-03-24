589cc7b9cf176fe0e47b094230165c9c5106f45e
almarzouk <jumaa.almarzouk@gmail.com>
2025-03-24 12:46:08 +0100

    build and redeploy

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,12 +5,12 @@ import Menu from "@/components/Menu/Menu";
 const BlogPage = ({ searchParams }) => {
   const page = parseInt(searchParams.page) || 1;
   const { cat } = searchParams;
-
+  // build and redeploy
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{cat} Blog</h1>
       <div className={styles.content}>
-        <CardList page={page} cat={cat}/>
+        <CardList page={page} cat={cat} />
         <Menu />
       </div>
     </div>
